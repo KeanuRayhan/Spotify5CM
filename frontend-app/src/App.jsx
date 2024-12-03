9c8a7dd7a843455ea0cf7956f4fe7f2c1df50b75
Revandi2245 <revandi.faudiamar.tif422@polban.ac.id>
2024-12-03 16:12:36 +0700

    music taste jsx

--- frontend-app/src/App.jsx
+++ frontend-app/src/App.jsx
@@ -4,7 +4,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-import Test from './pages/test'
+import Test from './pages/musicTaste'
 
 function App() {
   const [count, setCount] = useState(0)
