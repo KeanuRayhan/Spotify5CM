d7990e1bd8595adf8f249cbe0a4fd61f89ec9817
KeanuRayhan <keanu.rayhan.tif422@polban.ac.id>
2024-12-04 21:27:05 +0700

    Update app.js

--- back-end/app.js
+++ back-end/app.js
@@ -1,15 +1,15 @@
 const express = require('express');
+const connectDB = require('./db');
+const path = require('path');
 const matchRoutes = require('./routes/matchRoutes');
-const artistRoutes = require('./routes/artistRoutes');
-const connectDB = require('./db'); // Import connectDB
-
 const app = express();
 
-// Menjalankan koneksi ke MongoDB
 connectDB();
 
 app.use(express.json());
 app.use('/api', matchRoutes);
-app.use('/apidua', artistRoutes);
+
+// Sajikan gambar dari direktori assets
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.listen(3000, () => console.log('Server running on port 3000'));
