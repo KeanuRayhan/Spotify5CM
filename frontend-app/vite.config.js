a48f1b2ee83691ef2a2872d9d22a9083cbb69178
KeanuRayhan <keanu.rayhan.tif422@polban.ac.id>
2024-12-04 21:24:48 +0700

    Update vite.config.js

--- frontend-app/vite.config.js
+++ frontend-app/vite.config.js
@@ -4,14 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: '/home',
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://localhost:3000', // Alamat backend
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        secure: false,
       },
     },
   },
-})
+});
