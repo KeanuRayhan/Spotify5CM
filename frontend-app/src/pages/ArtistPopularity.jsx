bf101004627506661fb9fbf615b91e820e0ab122
MuhammadRizkiNurmuttaqin <muhammad.rizki.tif422@polban.ac.id>
2024-12-05 14:49:48 +0700

    pagination artist popularity

--- frontend-app/src/pages/ArtistPopularity.jsx
+++ frontend-app/src/pages/ArtistPopularity.jsx
@@ -4,13 +4,15 @@ import axios from 'axios';
 export default function ArtistPopularity() {
     const [artists, setArtists] = useState([]);
     const [region, setRegion] = useState('');
-    const [country, setCountry] = useState(''); 
+    const [country, setCountry] = useState('');
+    const [currentPage, setCurrentPage] = useState(1);
+    const [rowsPerPage, setRowsPerPage] = useState(10);
 
     // Fungsi untuk mengambil data dari backend dengan filter
     const fetchArtists = async () => { 
         try {
             const response = await axios.get('/apidua/artists/popularity', {
-                params: { region, country }, // Mengirim query parameter
+                params: { region, country },
             });
             setArtists(response.data);
         } catch (error) {
@@ -18,21 +20,41 @@ export default function ArtistPopularity() {
         }
     };
 
-    // useEffect untuk mengambil data dari backend setiap kali filter berubah
     useEffect(() => { 
         fetchArtists();
-    }, [region, country]); //  Memantau perubahan region dan country
+    }, [region, country]);
 
-    // Event handler untuk region
     const handleRegionChange = (e) => {
         setRegion(e.target.value);
-        setCountry(''); // Reset country ke default saat region berubah
+        setCountry(''); // Reset country saat region berubah
+        setCurrentPage(1); // Reset halaman ke 1
     };
 
-    // Event handler untuk country
     const handleCountryChange = (e) => {
         setCountry(e.target.value);
-        setRegion(''); // Reset region ke default saat country berubah
+        setRegion(''); // Reset region saat country berubah
+        setCurrentPage(1); // Reset halaman ke 1
+    };
+
+    // Pagination logic
+    const indexOfLastArtist = currentPage * rowsPerPage;
+    const indexOfFirstArtist = indexOfLastArtist - rowsPerPage;
+    const currentArtists = artists.slice(indexOfFirstArtist, indexOfLastArtist);
+
+    const totalPages = Math.ceil(artists.length / rowsPerPage);
+
+    const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+    const nextPage = () => {
+        if (currentPage < totalPages) {
+            setCurrentPage(currentPage + 1);
+        }
+    };
+
+    const prevPage = () => {
+        if (currentPage > 1) {
+            setCurrentPage(currentPage - 1);
+        }
     };
 
     return (
@@ -47,8 +69,8 @@ export default function ArtistPopularity() {
                             <select
                                 id="continent"
                                 className="mt-1 block w-full bg-violet-900 text-white font-bold px-4 py-2 rounded-full shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:border-indigo-600"
-                                value={region} // Menghubungkan state region
-                                onChange={handleRegionChange} // Mengubah state region
+                                value={region}
+                                onChange={handleRegionChange}
                             >
                                 <option value="">Select Region</option>
                                 <option value="Asia Tenggara">Asia Tenggara</option>
@@ -62,14 +84,14 @@ export default function ArtistPopularity() {
                             <select
                                 id="country"
                                 className="mt-1 block w-full bg-violet-900 text-white font-bold px-4 py-2 rounded-full shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:border-indigo-600"
-                                value={country} //  Menghubungkan state country
-                                onChange={handleCountryChange} //  Mengubah state country
+                                value={country}
+                                onChange={handleCountryChange}
                             >
                                 <option value="">Select Country</option>
                                 <option value="Amerika Serikat">Amerika Serikat</option>
                                 <option value="Jepang">Jepang</option>
                                 <option value="Korea Selatan">Korea Selatan</option>
-                                <option value="Philippines">Filipina</option>
+                                <option value="Filipina">Filipina</option>
                                 <option value="Malaysia">Malaysia</option>
                                 <option value="Indonesia">Indonesia</option>
                                 <option value="Singapura">Singapura</option>
@@ -81,19 +103,20 @@ export default function ArtistPopularity() {
                             </select>
                         </div>
                     </div>
-                </div>          
+                </div>
                 {/* Bagian Kanan (gambar) */}
                 <div className="bg-violet-400 text-center container w-1/2 p-4">
-                    <div className="p-8">  
+                    <div className="p-8">
                         <p className="text-slate-700 text-md font-bold">Discover the top artists trending across different countries and regions! See who's making waves and topping the charts around the world.</p>
-                    </div>  
+                    </div>
                 </div>
             </div>
 
+            {/* Table with Pagination */}
             <div className="mx-auto overflow-x-auto max-w-screen-lg m-4 bg-white shadow rounded-xl">
                 <table className="table-auto w-full text-left text-white">
                     <thead className="bg-indigo-950 text-white">
-                        <tr className="">
+                        <tr>
                             <th className="px-4 py-2">No</th>
                             <th className="px-4 py-2">Artist</th>
                             <th className="px-4 py-2">Monthly Listeners</th>
@@ -102,9 +125,9 @@ export default function ArtistPopularity() {
                         </tr>
                     </thead>
                     <tbody>
-                        {artists.map((artist, index) => (
+                        {currentArtists.map((artist, index) => (
                             <tr key={artist.id} className={index % 2 === 0 ? "bg-blue-950" : "bg-indigo-950"}>
-                                <td className="px-4 py-2">{index + 1}</td>
+                                <td className="px-4 py-2">{indexOfFirstArtist + index + 1}</td>
                                 <td className="px-4 py-2">
                                     <div className="flex items-center space-x-2">
                                         <img className="w-12 h-12 rounded-lg" src={artist.images} alt="album-cover" />
@@ -118,7 +141,50 @@ export default function ArtistPopularity() {
                         ))}
                     </tbody>
                 </table>
+
+                {/* Pagination Buttons */}
+                <div className="flex justify-center mt-4 space-x-2 items-center">
+                    <button
+                        onClick={() => setCurrentPage(1)}
+                        className="px-4 py-2 rounded font-bold text-white bg-indigo-500 hover:bg-indigo-700"
+                        disabled={currentPage === 1}
+                    >
+                        {"<<"}
+                    </button>
+                    <button
+                        onClick={prevPage}
+                        className="px-4 py-2 rounded font-bold text-white bg-indigo-500 hover:bg-indigo-700"
+                        disabled={currentPage === 1}
+                    >
+                        {"<"}
+                    </button>
+                    <div className="flex space-x-2">
+                        {Array.from({ length: totalPages }, (_, i) => (
+                            <button
+                                key={i}
+                                onClick={() => paginate(i + 1)}
+                                className={`px-4 py-2 rounded font-bold text-white ${currentPage === i + 1 ? 'bg-indigo-700' : 'bg-indigo-500'} hover:bg-indigo-700`}
+                            >
+                                {i + 1}
+                            </button>
+                        ))}
+                    </div>
+                    <button
+                        onClick={nextPage}
+                        className="px-4 py-2 rounded font-bold text-white bg-indigo-500 hover:bg-indigo-700"
+                        disabled={currentPage === totalPages}
+                    >
+                        {">"}
+                    </button>
+                    <button
+                        onClick={() => setCurrentPage(totalPages)}
+                        className="px-4 py-2 rounded font-bold text-white bg-indigo-500 hover:bg-indigo-700"
+                        disabled={currentPage === totalPages}
+                    >
+                        {">>"}
+                    </button>
+                </div>
             </div>
         </div>
     );
-};
+}
